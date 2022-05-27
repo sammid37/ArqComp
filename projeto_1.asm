6ab8e28105aa1ad2e3536f7e516f403bc9f18cb6
sammid37 <samanthadmedeiros37@gmail.com>
2022-05-27 13:03:33 -0300

    📓 Projeto 1

--- projeto_1.asm
+++ projeto_1.asm
@@ -5,8 +5,11 @@
     include \masm32\include\windows.inc
     include \masm32\include\kernel32.inc
     include \masm32\include\masm32.inc
+    include \masm32\include\msvcrt.inc
     includelib \masm32\lib\kernel32.lib
     includelib \masm32\lib\masm32.lib
+    includelib \masm32\lib\msvcrt.lib
+    include \masm32\macros\macros.asm
 .data
     ; lembrar de converter para ponto flutuante
     soma_notas dd 0 ;soma as notas do aluno
@@ -17,12 +20,17 @@
     msg_menu_1 db "1-Incluir notas de alunos", 0AH, 0
     msg_menu_2 db "2-Exibir médias de alunos", 0AH, 0
     msg_menu_3 db "3-Sair", 13,10, 0AH, 0
+
+    op_1_msg db "Vamos adicionar os alunos e as notas", 0AH, 0
+    op_2_msg db "Vamos exibir as medias", 0AH, 0
+    op_3_msg db "Obrigado por usar o sistema", 0AH, 0
     
     msg_opcao db "Digite a opção desejada: ", 0H
     msg_op_select db "A opção escolhida foi %d", 0AH, 0
     msg_limite db "Limite de alunos atingido.", 0AH, 0
-    opcao dd 4 dup(0) ; armazena a opção do usuário (resposta para msg_opcao)
+    opcao dd 4 dup(0) ; usigned int, armazena a opção do usuário (resposta para msg_opcao)
 
+    aux_handle dd 0 ; auxiliar de opção
     input_handle dd 0
     output_handle dd 0
     tamanho_string dd 0
@@ -30,40 +38,93 @@
 
 .code
 start:
-    ;------------Mensagem de Bem-vindo
-    invoke GetStdHandle, STD_OUTPUT_HANDLE 
-    mov output_handle, eax
-    invoke WriteConsole, output_handle, addr msg_sistema, sizeof msg_sistema, addr count, NULL
+    ;------------Mensagem de Bem-vindo e opções
+    menu:
+        invoke GetStdHandle, STD_OUTPUT_HANDLE 
+        mov output_handle, eax
+        invoke WriteConsole, output_handle, addr msg_sistema, sizeof msg_sistema, addr count, NULL
 
-    ; Exibindo opções (1 à 3)
-    invoke GetStdHandle, STD_OUTPUT_HANDLE 
-    mov output_handle, eax
-    invoke WriteConsole, output_handle, addr msg_menu_1, sizeof msg_menu_1, addr count, NULL
+        ; Exibindo opções (1 à 3)
+        invoke GetStdHandle, STD_OUTPUT_HANDLE 
+        mov output_handle, eax
+        invoke WriteConsole, output_handle, addr msg_menu_1, sizeof msg_menu_1, addr count, NULL
 
-    invoke GetStdHandle, STD_OUTPUT_HANDLE 
-    mov output_handle, eax
-    invoke WriteConsole, output_handle, addr msg_menu_2, sizeof msg_menu_2, addr count, NULL
+        invoke GetStdHandle, STD_OUTPUT_HANDLE 
+        mov output_handle, eax
+        invoke WriteConsole, output_handle, addr msg_menu_2, sizeof msg_menu_2, addr count, NULL
 
-    invoke GetStdHandle, STD_OUTPUT_HANDLE 
-    mov output_handle, eax
-    invoke WriteConsole, output_handle, addr msg_menu_3, sizeof msg_menu_3, addr count, NULL
-
-
-    ;------------Informando a opção
-
-    invoke GetStdHandle, STD_OUTPUT_HANDLE
-    mov output_handle, eax
-    
-    invoke GetStdHandle, STD_INPUT_HANDLE
-    mov input_handle, eax
+        invoke GetStdHandle, STD_OUTPUT_HANDLE 
+        mov output_handle, eax
+        invoke WriteConsole, output_handle, addr msg_menu_3, sizeof msg_menu_3, addr count, NULL
 
 
-    invoke StrLen, addr msg_opcao
-    mov tamanho_string, eax
-    invoke WriteConsole, output_handle, addr msg_opcao, tamanho_string, addr count, NULL
-    invoke ReadConsole, input_handle, addr opcao, sizeof opcao, addr count, NULL
+        ;------------Informando a opção
+        invoke GetStdHandle, STD_OUTPUT_HANDLE
+        mov output_handle, eax
 
-    ;------------ Comparando a opção digitada
+        invoke GetStdHandle, STD_INPUT_HANDLE
+        mov input_handle, eax
 
-    invoke ExitProcess, 0
+
+        invoke StrLen, addr msg_opcao
+        mov tamanho_string, eax
+        invoke WriteConsole, output_handle, addr msg_opcao, tamanho_string, addr count, NULL
+        invoke ReadConsole, input_handle, addr opcao, sizeof opcao, addr count, NULL
+
+        ; Convertendo para inteiro
+        COMMENT @
+        mov esi, offset opcao
+        proximo:
+            mov al, [esi]
+            inc esi
+            cmp al, 48
+            jl terminar
+            cmp al, 58
+            jl proximo
+        terminar:
+            dec esi
+            xor al, al
+            mov [esi], al
+
+        invoke atodw, offset opcao ; transf a str de opcao em int e guarda em eax
+        invoke dwtoa, eax, offset opcao ; faz o inverso
+
+        invoke StrLen, offset opcao
+        mov tamanho_string, eax
+        invoke WriteConsole, output_handle, offset opcao, tamanho_string, offset count, NULL
+        @
+        
+        ;------Realizando a comparação
+        comparar:
+            ;--Adicionar Notas
+            cmp opcao, 49
+            jne exibir_medias 
+
+            ;...... Funcionalidades da opção 1
+            invoke GetStdHandle, STD_OUTPUT_HANDLE 
+            mov output_handle, eax
+            invoke WriteConsole, output_handle, addr op_1_msg, sizeof op_1_msg, addr count, NULL
+
+            ;--Exibe as médias
+            exibir_medias:
+                cmp opcao, 50
+                jne sair ; se não for igual, vai para a opção 3, sair
+
+                ;...... Funcionalidades da opção 2
+                invoke GetStdHandle, STD_OUTPUT_HANDLE 
+                mov output_handle, eax
+                invoke WriteConsole, output_handle, addr op_2_msg, sizeof op_2_msg, addr count, NULL
+       
+            ;--Encerra o Programa
+            sair:
+                cmp opcao, 51
+                ;jne comparar
+                
+                ;...... Funcionalidades da opção 3
+                invoke GetStdHandle, STD_OUTPUT_HANDLE 
+                mov output_handle, eax
+                invoke WriteConsole, output_handle, addr op_3_msg, sizeof op_3_msg, addr count, NULL
+
+
+            invoke ExitProcess, 0
 end start
