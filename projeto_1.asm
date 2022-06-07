df3a10c386525ffa755afb501f190d8dcc79d48f
sammid37 <samanthadmedeiros37@gmail.com>
2022-06-07 13:53:04 -0300

    📓 Projeto 1

--- projeto_1.asm
+++ projeto_1.asm
@@ -1,5 +1,5 @@
 ; Samantha Dantas Medeiros (GitHub: @sammid37)
-; 27/05/2022
+; 05/06/2022
 .686
 .model flat, stdcall
     option casemap: none
@@ -17,10 +17,10 @@
 .data
     ;------Variaveis referentes ao menu de opcaoes
     ; Mensasagens do Menu
-    msg_sistema db "BEM-VINDO AO SISTEMA DE NOTAS", 0AH, 0
-    msg_menu_1 db "1-Incluir notas de alunos", 0AH, 0
-    msg_menu_2 db "2-Exibir medias de alunos", 0AH, 0
-    msg_menu_3 db "3-Sair", 13,10, 0AH, 0
+    msg_sistema db "BEM-VINDO AO SISTEMA DE NOTAS", 0AH
+    msg_menu_1 db "1-Incluir notas de alunos", 0AH
+    msg_menu_2 db "2-Exibir medias de alunos", 0AH
+    msg_menu_3 db "3-Sair", 13,10, 0AH
     
     msg_opcao db "Digite a opcao desejada: ", 0H ; unsigned char, pergunta
     opcao db 5 dup(0) ; usigned char, armazena a opcao do usuario (resposta para msg_opcao)
@@ -31,42 +31,46 @@
     op_3_msg db "Obrigado por usar o sistema", 0AH, 0
     
     input_handle dd 0 ; handle para a entrada de dados
-    output_handle dd 0 ; handle para a sai­da de dados
+    output_handle dd 0 ; handle para a saiï¿½da de dados
     tamanho_string dd 0
     count dd 0
     
     ;------Variaveis referentes a opcao 1 
     ; Mensagens (com e sem quebra de linha)
     msg_nome db "Insira o nome do aluno: ", 0H, 0
-    msg_nota_1 db "Nota 1: ", 0H, 0
-    msg_nota_2 db "Nota 2: ", 0H, 0
-    msg_nota_3 db "Nota 3: ", 0H, 0
+    msg_nota_1 db "Nota 1: ", 0H
+    msg_nota_2 db "Nota 2: ", 0H
+    msg_nota_3 db "Nota 3: ", 0H
     msg_limite db "Limite de alunos atingido.", 0AH, 0
 
     nome_aluno db 15 dup(0) ; armazena o nome do aluno
-    nota db 20 dup(0) ; armazena uma nota temporariamente, será convertido para float (8)
-
-    nota_float8 REAL8 0.0 ; nota real 8 temporaria que será passada para os arrays
+    nota db 20 dup(0) ; armazena uma nota temporariamente, sera convertido para float (8)
+    nota_float8 REAL8 0.0 ; nota real 8 temporaria que sera passada para os arrays
     
     lista_nomes db 600 dup (0) ; 15 * 4 bytes
     n1 REAL8 40 dup(0.0)
     n2 REAL8 40 dup(0.0)
     n3 REAL8 40 dup(0.0)
+    novanota REAL8 0.0
 
     msg_digitou db "Digitou: ", 0H, 0
     
-    qtd_alunos dd 0, 0 ; contador, armazena a qtd de alunos
-    indice_nota dd 0, 0 ; auxiliar para insercao de notas em arrays
-    indice_aluno dd 0, 0 ; auxiliar para a insercao de nome em array
+    qtd_alunos dd 0 ; contador, armazena a qtd de alunos
+    indice_nota_4 dd 0 ; auxiliar para insercao de notas em arrays (REAL4)
+    indice_nota_8 dd 0 ; auxiliar para insercao de notas em arrays (REAL8)
+    indice_aluno dd 0 ; auxiliar para a insercao de nome em array
+    
 
     ;------Variaveis referentes a opcao 2
     msg_qtd_alunos db "Quantidade de alunos registrados: ", 0H, 0
     alunos db 15 dup(0) ; sera usado para converter qtd_alunos para string
     
     ; Array de tamanho 40 preenchido com 0.0
-    nota1 REAL4 40 dup(7.0) ; armazena a nota 1
-    nota2 REAL4 40 dup(8.0) ; armazena a nota 2
-    nota3 REAL4 40 dup(9.0) ; armazena a nota 3  
+    teste REAL4 5.0
+    teste2 REAL8 0.0
+    nota1 REAL4 40 dup(0.0) ; armazena a nota 1
+    nota2 REAL4 40 dup(0.0) ; armazena a nota 2
+    nota3 REAL4 40 dup(0.0) ; armazena a nota 3  
 
     ; Arrays para as 40 somas de notas e as 40 medias (todoso preenchidos com 0.0)
     qtd_notas REAL4 40 dup(3.0) ; sera usado para dividir a soma das notas (n1+n2+n3)/3
@@ -84,9 +88,7 @@ start:
     xor eax, eax ; zerando ebx
 
     ; pilha de exec, inserir notas
-
     ; pilha de exec, somar notas
-
     ; pilha de exec, calcular media
 
     ;------Handles para a entrada e saida de dados
@@ -109,7 +111,7 @@ start:
         mov tamanho_string, eax
         invoke WriteConsole, output_handle, offset msg_opcao, sizeof msg_opcao, offset count, NULL
         invoke ReadConsole, input_handle, offset opcao, sizeof opcao, offset count, NULL
-
+        
         ;------Realizando a comparacao
         ;--Adicionar Notas
         cmp opcao, 49 ; 49 (ASCII) = 1 (DEC)
@@ -133,35 +135,72 @@ start:
                 mov eax, indice_aluno
                 invoke WriteConsole, output_handle, offset msg_nome-1, sizeof msg_nome-1, offset count, NULL
                 invoke ReadConsole, input_handle, offset nome_aluno, sizeof nome_aluno, offset count, NULL
-                ;.Adicionar nome ao array de nomes
+                ;********************.Adicionar nome ao array de nomes
                 ;mov DWORD PTR [lista_nomes+eax], nome_aluno
                 ;mov lista_nomes[eax], nome_aluno
                 ;printf("Nomes: %\n",lista_nomes[eax])
                 ;invoke WriteConsole, output_handle, offset nome_aluno, sizeof nome_aluno, offset count, NULL
                 
                 ;..Informando as notas
+                ; As notas foram inseridas através da multiplicacao de indice ao inves da soma
+                ; Pois eu estava, por algum motivo desconhecido, perdendo todas notas que eu ja havia preenchido (com o metodo da soma de indices)
                 ;.nota 1
-                mov ebx, indice_nota
+                mov eax, qtd_alunos
+                mov ebx, 8
+                mul ebx
+                mov ebx, eax
+                push ebx ; coloca no topo da pilha
                 invoke WriteConsole, output_handle, offset msg_nota_1, sizeof msg_nota_1, offset count, NULL
-                invoke ReadConsole, input_handle, offset nota, sizeof nota, offset count, NULL   
-                invoke StrToFloat, offset nota, addr[n1+ebx] ; Converte a str de nota para float (real8) no array n1       
-              
+                invoke ReadConsole, input_handle, offset nota, sizeof nota, offset count, NULL
+                pop ebx
+                push ebx   
+                invoke StrToFloat, offset nota, addr[n1+ebx] ; Converte a str de nota para float (real8) no array n1
+                mov eax, qtd_alunos
+                mov ebx, 4
+                mul ebx
+                pop ebx ; remove do topo da pilha
+                fld REAL8 PTR[n1+ebx] ; carrega o valor do array REAL 8
+                fstp REAL4 PTR[nota1+eax] ; preenche o vetor REAL4 com o valor do array REAL 8 
+                
                 ;.nota 2
-                mov ebx, indice_nota
+                mov eax, qtd_alunos
+                mov ebx, 8
+                mul ebx
+                mov ebx, eax
+                push ebx ; coloca no topo da pilha
                 invoke WriteConsole, output_handle, offset msg_nota_2, sizeof msg_nota_2, offset count, NULL
-                invoke ReadConsole, input_handle, offset nota, sizeof nota, offset count, NULL         
+                invoke ReadConsole, input_handle, offset nota, sizeof nota, offset count, NULL
+                pop ebx
+                push ebx          
                 invoke StrToFloat, offset nota, addr[n2+ebx] ; Converte a str de nota para float (real8) no array n2
-      
+                mov eax, qtd_alunos
+                mov ebx, 4
+                mul ebx
+                pop ebx ; remove do topo da pilha
+                fld REAL8 PTR[n2+ebx] ; carrega o valor do array REAL 8
+                fstp REAL4 PTR[nota2+eax] ; preenche o vetor REAL4 com o valor do array REAL 8
+               
                 ;.nota 3
-                mov ebx, indice_nota
+                mov eax, qtd_alunos
+                mov ebx, 8
+                mul ebx
+                mov ebx, eax
+                push ebx ; coloca no topo da pilha
                 invoke WriteConsole, output_handle, offset msg_nota_3, sizeof msg_nota_3, offset count, NULL
                 invoke ReadConsole, input_handle, offset nota, sizeof n3, offset count, NULL
-                invoke StrToFloat, offset nota, addr[n3+ebx] ; Converte a str de nota para float (real8) no array n3
-                
+                pop ebx
+                push ebx
+                invoke StrToFloat, offset nota, addr[n3+ebx] ; Converte a str de nota para float (real8) no array n3 
+                mov eax, qtd_alunos
+                mov ebx, 4
+                mul ebx
+                pop ebx ; remove do topo da pilha
+                fld REAL8 PTR[n3+ebx] ; carrega o valor do array REAL 8
+                fstp REAL4 PTR[nota3+eax] ; preenche o vetor REAL4 com o valor do array REAL 8
+                               
                 ;..Incrementando o contador de alunos e os indices
                 add qtd_alunos, 1
                 add indice_aluno, 4 ; +4, pois o array de nome eh do tipo DWORD
-                add indice_nota, 8 ; +8, pois os array n1, n2 e n3 sao do tipo REAL8)
                 
         ;--Exibe as medias
         exibir_medias:
@@ -174,19 +213,14 @@ start:
             invoke dwtoa, qtd_alunos, addr alunos ; converte int para string
             invoke WriteConsole, output_handle, addr alunos, sizeof alunos, addr count, NULL ; qtd de alunos cadastrados
             invoke WriteConsole, output_handle, addr quebra_linha, sizeof quebra_linha, addr count, NULL ; \n
-            
-            ; Inserido os valores do arrays REAL8 para os arrays REAL4
-            ;invoke FloatToStr, n1, nota1
-            ;invoke FloatToStr, n2, nota2
-            ;invoke FloatToStr, n3, nota3
-            
+               
             ;....Funcoes SSE
             ;..Movendo os arrays REAL4 para os registradores
-            mov eax, indice_aluno
+            mov edx, indice_aluno
 
-            movups xmm0, OWORD PTR[nota1+eax]  
-            movups xmm1, OWORD PTR[nota2+eax]
-            movups xmm2, OWORD PTR[nota3+eax]
+            movups xmm0, OWORD PTR[nota1+edx]  
+            movups xmm1, OWORD PTR[nota2+edx]
+            movups xmm2, OWORD PTR[nota3+edx]
             
             ;.Somando as notas
             addps xmm0, xmm1
@@ -195,16 +229,22 @@ start:
             
             ;.Calculando a media
             divps xmm0, xmm3 ; dividindo a soma das 3 notas por 3
-            movups DWORD PTR[media+eax], xmm0 ; move a soma armazenada em xmm0 para o array media (REAL4)
+            movups DWORD PTR[media+edx], xmm0 ; move a soma armazenada em xmm0 para o array media (REAL4)
 
             fld DWORD PTR[media+4]
             fstp QWORD PTR[media_aluno] ; REAL8
+
+            ;teste
+            fld REAL8 PTR[n1]
+            fstp REAL4 PTR[nota1]
+            
             
             ;..Impressao: 
             printf("NOME: ")
             invoke WriteConsole, output_handle, addr nome_aluno, sizeof nome_aluno, addr count, NULL ; nome do aluno
            
             printf("NOTAS: %f %f %f \n", n1[0], n2[0], n3[0])
+            printf("NOTAS: %f %f %f \n", n1[8], n2[8], n3[8])
             ;invoke WriteConsole, output_handle, addr n1, sizeof n1, addr count, NULL ; n1
             ;invoke WriteConsole, output_handle, addr n2, sizeof n2, addr count, NULL ; n2
             ;invoke WriteConsole, output_handle, addr n3, sizeof n3, addr count, NULL ; n3
